d13fd01ce2d5664456bdf56f2252ddc0abbdc9b5
mohsin-r <mohsin.reza@mail.utoronto.ca>
2023-04-24 06:28:36 -0400

    Add i18n

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import './assets/main.css'
+import { i18n } from './lang'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(i18n())
 
 app.mount('#app')
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,5 @@
+declare module '*lang.csv' {
+  import { LocaleMessages, VueMessageType } from 'vue-i18n'
+  const content: LocaleMessages<VueMessageType>
+  export default content
+}
