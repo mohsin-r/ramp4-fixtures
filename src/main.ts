da1484a4cd76861929ae168b2461fd6fdae8a973
mohsin-r <mohsin.reza@mail.utoronto.ca>
2023-05-15 15:58:21 -0400

    Cant get it to work

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { panelScreen } from './lib/ramp.esm'
 import App from './App.vue'
 
-import './assets/main.css'
-import { i18n } from './lang'
+import './styles/main.css'
+import './lib/ramp.css'
+// import { i18n } from './lang'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(i18n())
+// app.use(i18n())
 
-app.mount('#app')
+app.mount('#ramp4-editor')
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 // https://vitejs.dev/config/
 export default defineConfig({
